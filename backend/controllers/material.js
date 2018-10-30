4b784a117c6a30eddbcd81bb75a1d2b9c56f8bd7
ptorresmanque <ptorresmanque@gmail.com>
2018-10-30 15:50:08 -0300

    funciones malas

--- backend/controllers/material.js
+++ backend/controllers/material.js
@@ -3,6 +3,24 @@ function Prueba(req, res) {
     res.status(200).send({ tetxo: "hola mundo" });
 };
 
+
+function getPM(req, res) {
+
+}
+
+function getPM(req, res) {
+
+}
+
+function getPM(req, res) {
+
+}
+
+function getPM(req, res) {
+
+}
+
+
 module.exports = {
     Prueba
 }
