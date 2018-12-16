c2ee71af90d127cb7ea9c8a93db259f1e27b462e
Patricio <ptorresmanque@gmail.com>
2018-12-16 01:57:15 -0300

    cuatro graficos

--- front/grafica/src/app/components/grafica/grafica.component.ts
+++ front/grafica/src/app/components/grafica/grafica.component.ts
@@ -8,13 +8,26 @@ import { WebsocketService } from '../../services/websocket.service';
   styleUrls: ['./grafica.component.css']
 })
 export class GraficaComponent implements OnInit {
-  public lineChartData: Array<any> = [
+  public lineChartDataTemperatura: Array<any> = [
     {data: [], label: 'Temperatura'}
   ];
+  public lineChartDataHumedad: Array<any> = [
+    {data: [], label: 'Humedad'}
+  ];
+  public lineChartDataPM10: Array<any> = [
+    {data: [], label: 'PM10'}
+  ];
+  public lineChartDataPM25: Array<any> = [
+    {data: [], label: 'PM2.5'}
+  ];
+
   public lineChartLabels: Array<String> = [];
   constructor(private http: HttpClient, public wsService: WebsocketService) { }
 
   public temperatura: Array<number> = new Array();
+  public humedad: Array<number> = new Array();
+  public pm10: Array<number> = new Array();
+  public pm25: Array<number> = new Array();
   public fechas: Array<any> = new Array();
 
   ngOnInit() {
@@ -30,8 +43,10 @@ export class GraficaComponent implements OnInit {
           const muestra = data[i];
           for (const j in muestra) {
             if (muestra.hasOwnProperty(j)) {
-              const element =  Number(muestra[j].Temperatura);
-              this.temperatura.push(element);
+              this.temperatura.push(muestra[j].Temperatura);
+              this.humedad.push(muestra[j].Humedad);
+              this.pm10.push(muestra[j].PM10);
+              this.pm25.push(muestra[j].PM25);
               this.lineChartLabels.push(muestra[j].date);
             }
           }
@@ -39,8 +54,20 @@ export class GraficaComponent implements OnInit {
       }
       this.lineChartLabels.reverse();
       this.temperatura.reverse();
-      this.lineChartData = [
+      this.humedad.reverse();
+      this.pm10.reverse();
+      this.pm25.reverse();
+      this.lineChartDataTemperatura = [
         {data: this.temperatura, label: 'Temperatura'}
+      ];
+      this.lineChartDataHumedad = [
+        {data: this.humedad, label: 'Humedad'}
+      ];
+      this.lineChartDataPM10 = [
+        {data: this.pm10, label: 'PM10'}
+      ];
+      this.lineChartDataPM25 = [
+        {data: this.pm25, label: 'PM2.5'}
       ];
     });
   }
@@ -55,14 +82,41 @@ export class GraficaComponent implements OnInit {
         this.temperatura.pop();
         this.temperatura.reverse();
       }
+      this.humedad.push(data.Humedad);
+      if (this.humedad.length > 1000) {
+        this.humedad.reverse();
+        this.humedad.pop();
+        this.humedad.reverse();
+      }
+      this.pm10.push(data.PM10);
+      if (this.pm10.length > 1000) {
+        this.pm10.reverse();
+        this.pm10.pop();
+        this.pm10.reverse();
+      }
+      this.pm25.push(data.PM25);
+      if (this.pm25.length > 1000) {
+        this.pm25.reverse();
+        this.pm25.pop();
+        this.pm25.reverse();
+      }
       this.lineChartLabels.push(data.date);
       if (this.lineChartLabels.length > 1000) {
         this.lineChartLabels.reverse();
         this.lineChartLabels.pop();
         this.lineChartLabels.reverse();
       }
-      this.lineChartData = [
+      this.lineChartDataTemperatura = [
         {data: this.temperatura, label: 'Temperatura'}
+      ];
+      this.lineChartDataHumedad = [
+        {data: this.humedad, label: 'Humedad'}
+      ];
+      this.lineChartDataPM10 = [
+        {data: this.pm10, label: 'PM10'}
+      ];
+      this.lineChartDataPM25 = [
+        {data: this.pm25, label: 'PM2.5'}
       ];
     });
   }
